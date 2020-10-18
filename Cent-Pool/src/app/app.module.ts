19e6271618b637b005a31c96f408df2f2c0c9641
Dengson12 <45352046+Dengson12@users.noreply.github.com>
2020-10-18 15:37:52 +0300

    bare stages

--- Cent-Pool/src/app/app.module.ts
+++ Cent-Pool/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
